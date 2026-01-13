c15334561f481e4fc96ff2de747bf6c8acac1c1b
Andre Januario <ahenrique.ah70@gmail.com>
2026-01-12 22:04:34 -0300

    Aula Double

---
+++ lib/Tipos/double.dart
@@ -0,0 +1,18 @@
+void main(){
+  double peso = 72.5;
+
+  print("Peso: $peso");
+
+  double precoNotebook = 3999.99;
+
+  print("Preço notebook: ${precoNotebook.toStringAsFixed(2)}");
+
+
+  double saldoBancario = 5000.00;
+
+  double saldoPosCompra = saldoBancario - precoNotebook;
+
+  print("Saldo apos a compra: $saldoPosCompra");
+
+
+}
