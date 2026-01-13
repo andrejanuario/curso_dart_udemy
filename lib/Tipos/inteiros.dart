b7dc4064cc458ba925419e13f5d75c9f56dac0b6
Andre Januario <ahenrique.ah70@gmail.com>
2026-01-12 21:46:03 -0300

    Aula Inteiros

---
+++ lib/Tipos/inteiros.dart
@@ -0,0 +1,24 @@
+void main (){
+    int altura = 183;
+
+    print("Altura: $altura cm");
+
+    int ano = 2025;
+
+    print("Ano atual: $ano");
+
+
+    int idade = 27;
+
+    print("Idade: $idade");
+
+
+    int dataNascimento = ano - idade;
+
+
+    print("Data de nascimento: $dataNascimento");
+    
+
+
+
+}
---
+++ lib/Tipos/variaveis.dart
@@ -0,0 +1,10 @@
+void main(){
+  var nome = "Paulo";
+
+  nome = "Paulo Mendes";
+  print(nome);
+  
+  var idade = 27;
+  print(idade);
+  
+}
